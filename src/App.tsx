7d0cc431521092ad10b2f5a7d5422b65c6ea3560
James Marwell <jcmarwell@gmail.com>
2023-09-05 11:01:32 -0600

    basic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 export default function App(){
+  const name:string = 'Jamie';
+  const isLoggedIn:boolean = true;
+  const posts: {id:number, title:string}[] = [
+    {id: 1, title: 'Happy Tuesday'},
+    {id: 2, title: 'How was your weekend?'},
+    {id: 3, title: 'I love React!'}
+  ];
   return(
-    <div>Hello World</div>
+    <div>
+      {isLoggedIn ? <h1>Hello {name}!</h1>:<h1>Hello and welcome!</h1>}
+      {posts.map(p => <li key={p.id}>{p.title}</li>)}
+    </div>
   )
 }
