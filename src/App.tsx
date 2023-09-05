1be55c3b0edf1c340ab71af9b2f7c7d455ae9f76
James Marwell <jcmarwell@gmail.com>
2023-09-05 13:15:45 -0600

    added navbar begining

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-export default function App(){
-  const name:string = 'Jamie';
-  const isLoggedIn:boolean = true;
-  const posts: {id:number, title:string}[] = [
-    {id: 1, title: 'Happy Tuesday'},
-    {id: 2, title: 'How was your weekend?'},
-    {id: 3, title: 'I love React!'}
-  ];
-  return(
-    <div>
-      {isLoggedIn ? <h1>Hello {name}!</h1>:<h1>Hello and welcome!</h1>}
-      {posts.map(p => <li key={p.id}>{p.title}</li>)}
-    </div>
-  )
+import Navigation from "./components/Navigation";
+
+export default function App() {
+    const name:string = 'Jamie';
+    const isLoggedIn:boolean = false;
+
+    const posts: {id:number, title:string}[] = [
+        {id: 1, title: 'Happy Tuesday'},
+        {id: 2, title: 'How was your weekend?'},
+        {id: 3, title: 'I love React!'}
+    ];
+
+    return (
+        <div>
+            <Navigation isLoggedIn={isLoggedIn}/>
+            { isLoggedIn ? <h1>Hello {name}!</h1> : <h1>Hello and Welcome</h1>}
+            {posts.map(p => <li key={p.id}>{p.title}</li>)}
+        </div>
+    )
 }
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,31 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+
+type NavigationProps = {
+    isLoggedIn: boolean
+}
+
+export default function Navigation({ isLoggedIn }:NavigationProps) {
+    
+    return (
+        <Navbar bg='dark' data-bs-theme='dark'>
+            <Container>
+                <Navbar.Brand href='/'>React Blog</Navbar.Brand>
+                <Nav className='me-auto'>
+                    { isLoggedIn ? (
+                        <>
+                            <Nav.Link href='/'>Create Post</Nav.Link>
+                            <Nav.Link href='/'>Log Out</Nav.Link>
+                        </>
+                    ) : (
+                        <>
+                            <Nav.Link href='/'>Sign Up</Nav.Link>
+                            <Nav.Link href='/'>Log In</Nav.Link>
+                        </>
+                    )}
+                </Nav>
+            </Container>
+        </Navbar>
+    )
+}
